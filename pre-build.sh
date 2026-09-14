abca15c13f8ea60d65b3fb001071d2cf77e9806c
Jon-Smou <Bedniu8@gmail.com>
2026-09-14 20:07:22 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -2,24 +2,26 @@
 
 echo "=== НАЧАЛО ИНТЕГРАЦИИ ZAPRET 2 ==="
 
-# 1. Скачиваем чистые исходники Zapret 2 во временную папку
-git clone --depth 1 https://github.com /tmp/zapret2
+# Фикс для старых версий git в контейнерах сборщика
+git config --global url."https://github.com/".insteadOf "git@github.com:"
 
-# 2. Переходим в папку с исходным кодом nfqws
-cd /tmp/zapret2/nfq2
+# 1. Скачиваем чистые исходники Zapret 2 во временную папку (с явным указанием ветки master)
+git clone -b master https://github.com /tmp/zapret2
+
+# 2. Переходим в папку с исходным кодом nfq (исправлено имя каталога)
+cd /tmp/zapret2/nfq
 
 # 3. Собираем бинарник nfqws2 под архитектуру роутера (используем тулчейн Padavan)
-# В контейнере padavan-ng кросс-компилятор mipsel-linux-uclibc-gcc уже установлен в PATH
 make CC=mipsel-linux-uclibc-gcc STRIP=mipsel-linux-uclibc-strip
 
-# 4. Проверяем, собрался ли файл nfqws2
-if [ -f nfqws2 ]; then
+# 4. Проверяем, собрался ли файл nfqws
+if [ -f nfqws ]; then
     echo "=== Сборка nfqws2 прошла успешно! Подменяем файлы... ==="
     
-    # Заменяем старый бинарник nfqws в исходниках прошивки на новый nfqws2
-    cp nfqws2 $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin/nfqws
+    # Заменяем старый бинарник nfq в исходниках прошивки на новый собранный nfqws2
+    cp nfqws $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin/nfqws
     
-    # На всякий случай даем права на исполнение
+    # Даем права на исполнение
     chmod +x $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin/nfqws
 else
     echo "=== ОШИБКА: Не удалось скомпилировать nfqws2! ==="
