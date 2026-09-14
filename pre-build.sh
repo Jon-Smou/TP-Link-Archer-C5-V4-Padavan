5928e974c5d9f8395c80073198295d9682ea4eda
Jon-Smou <Bedniu8@gmail.com>
2026-09-14 20:17:11 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -2,27 +2,30 @@
 
 echo "=== НАЧАЛО ИНТЕГРАЦИИ ZAPRET 2 ==="
 
-# 1. Скачиваем исходники Zapret 2 в виде стабильного ZIP-архива с помощью wget
-wget -qO /tmp/zapret2.zip https://github.com
+# 1. Скачиваем чистый архив Zapret 2 с помощью curl, обрабатывая редиректы
+curl -sL https://github.com -o /tmp/zapret2.tar.gz
 
-# 2. Распаковываем архив во временную папку
-unzip -q /tmp/zapret2.zip -d /tmp/
+# 2. Распаковываем tar.gz архив (он надежнее zip-архивов в этой среде)
+tar -xzf /tmp/zapret2.tar.gz -C /tmp/
 
-# 3. Переходим в папку с исходным кодом nfq (у папки из архива имя будет 'zapret2-master')
+# 3. Переходим в папку с исходным кодом nfq (внутри архива папка называется zapret2-master)
 cd /tmp/zapret2-master/nfq
 
-# 4. Собираем бинарник nfqws под архитектуру роутера (используем тулчейн Padavan)
+# 4. Собираем бинарник nfqws под процессор роутера (MIPS)
 make CC=mipsel-linux-uclibc-gcc STRIP=mipsel-linux-uclibc-strip
 
-# 5. Проверяем, собрался ли файл nfqws
+# 5. Проверяем, собран ли файл, и подменяем его в дереве исходников Padavan
 if [ -f nfqws ]; then
     echo "=== Сборка nfqws2 прошла успешно! Подменяем файлы... ==="
     
-    # Заменяем старый бинарник nfq в исходниках прошивки на новый собранный nfqws2
-    cp nfqws $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin/nfqws
+    # Находим точный путь к папке встроенного запрета в репозитории padavan-ng
+    TARGET_DIR=$(find $GITHUB_WORKSPACE -type d -name "zapret" | grep "trunk/user/zapret" | head -n 1)
     
-    # Даем права на исполнение
-    chmod +x $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin/nfqws
+    # Копируем свежий скомпилированный бинарник на место старого
+    cp nfqws "$TARGET_DIR/bin/nfqws"
+    chmod +x "$TARGET_DIR/bin/nfqws"
+    
+    echo "Файл успешно заменен в: $TARGET_DIR/bin/nfqws"
 else
     echo "=== ОШИБКА: Не удалось скомпилировать nfqws2! ==="
     exit 1
