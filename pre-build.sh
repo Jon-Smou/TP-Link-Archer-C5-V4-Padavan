a99397395aa2b796bac8f014e486b771749efb8c
Jon-Smou <Bedniu8@gmail.com>
2026-09-14 20:20:10 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 
-echo "=== НАЧАЛО ИНТЕГРАЦИИ ZAPRET 2 ==="
+echo "=== НАЧАЛО КОМПИЛЯЦИИ ZAPRET 2 ==="
 
-# 1. Скачиваем чистый архив Zapret 2 с помощью curl, обрабатывая редиректы
-curl -sL https://github.com -o /tmp/zapret2.tar.gz
+# Переходим в папку с исходным кодом nfq, который уже подготовил GitHub
+cd $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/nfq
 
-# 2. Распаковываем tar.gz архив (он надежнее zip-архивов в этой среде)
-tar -xzf /tmp/zapret2.tar.gz -C /tmp/
-
-# 3. Переходим в папку с исходным кодом nfq (внутри архива папка называется zapret2-master)
-cd /tmp/zapret2-master/nfq
-
-# 4. Собираем бинарник nfqws под процессор роутера (MIPS)
+# Собираем бинарник nfqws под процессор роутера (MIPS)
 make CC=mipsel-linux-uclibc-gcc STRIP=mipsel-linux-uclibc-strip
 
-# 5. Проверяем, собран ли файл, и подменяем его в дереве исходников Padavan
+# Проверяем, собран ли файл, и копируем его в нужную папку
 if [ -f nfqws ]; then
     echo "=== Сборка nfqws2 прошла успешно! Подменяем файлы... ==="
-    
-    # Находим точный путь к папке встроенного запрета в репозитории padavan-ng
-    TARGET_DIR=$(find $GITHUB_WORKSPACE -type d -name "zapret" | grep "trunk/user/zapret" | head -n 1)
-    
-    # Копируем свежий скомпилированный бинарник на место старого
-    cp nfqws "$TARGET_DIR/bin/nfqws"
-    chmod +x "$TARGET_DIR/bin/nfqws"
-    
-    echo "Файл успешно заменен в: $TARGET_DIR/bin/nfqws"
+    cp nfqws $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin/nfqws
+    chmod +x $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin/nfqws
+    echo "Файл успешно заменен!"
 else
     echo "=== ОШИБКА: Не удалось скомпилировать nfqws2! ==="
     exit 1
 fi
 
-echo "=== ЗАВЕРШЕНИЕ ИНТЕГРАЦИИ ZAPRET 2 ==="
+echo "=== ЗАВЕРШЕНИЕ КОМПИЛЯЦИИ ZAPRET 2 ==="
