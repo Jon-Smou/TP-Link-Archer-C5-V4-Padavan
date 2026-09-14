d31315176ce72bf3d28f5d54f75fbec20094b6ce
Jon-Smou <Bedniu8@gmail.com>
2026-09-14 20:24:37 +0300

    Update pre-build.sh

--- pre-build.sh
+++ pre-build.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-echo "=== НАЧАЛО КОМПИЛЯЦИИ ZAPRET 2 ==="
+echo "=== НАЧАЛО ИНТЕГРАЦИИ ZAPRET 2 ==="
 
-# Переходим в папку с исходным кодом nfq, который уже подготовил GitHub
+# Скачиваем архив исходников с помощью Python (он встроен в контейнер и работает со 100% стабильностью сети)
+python3 -c "import urllib.request; urllib.request.urlretrieve('https://github.com', '/tmp/z2.zip')"
+
+# Распаковываем архив
+unzip -q /tmp/z2.zip -d /tmp
+
+# Создаем структуру папок в репозитории Padavan, если она еще не создана автоматикой
+mkdir -p $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret
+cp -r /tmp/zapret2-master/nfq $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/
+
+# Переходим в папку и компилируем nfqws2
 cd $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/nfq
-
-# Собираем бинарник nfqws под процессор роутера (MIPS)
 make CC=mipsel-linux-uclibc-gcc STRIP=mipsel-linux-uclibc-strip
 
-# Проверяем, собран ли файл, и копируем его в нужную папку
+# Проверяем успешность компиляции и подменяем оригинальный файл
 if [ -f nfqws ]; then
-    echo "=== Сборка nfqws2 прошла успешно! Подменяем файлы... ==="
+    echo "=== Сборка nfqws2 прошла успешно! ==="
+    mkdir -p $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin
     cp nfqws $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin/nfqws
     chmod +x $GITHUB_WORKSPACE/padavan-ng/trunk/user/zapret/bin/nfqws
-    echo "Файл успешно заменен!"
+    echo "Файл Zapret 2 успешно вшит!"
 else
-    echo "=== ОШИБКА: Не удалось скомпилировать nfqws2! ==="
+    echo "=== ОШИБКА: Компиляция nfqws2 провалилась! ==="
     exit 1
 fi
 
-echo "=== ЗАВЕРШЕНИЕ КОМПИЛЯЦИИ ZAPRET 2 ==="
+echo "=== ЗАВЕРШЕНИЕ ИНТЕГРАЦИИ ZAPRET 2 ==="
